8dd054a515964ca8980f1993efeee4661759e094
dsame <dsame@github.com>
2024-06-13 16:58:50 +0200

    3.0.0-alpha1 Compatible with iTag alpha

--- waytoday-sdk/src/main/java/solutions/s4y/waytoday/sdk/AndroidWayTodayClient.java
+++ waytoday-sdk/src/main/java/solutions/s4y/waytoday/sdk/AndroidWayTodayClient.java
@@ -1,6 +1,7 @@
 package solutions.s4y.waytoday.sdk;
 
 import android.content.Context;
+import android.os.Looper;
 
 import androidx.annotation.NonNull;
 import androidx.work.Data;
@@ -10,20 +11,77 @@ import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
-/** @noinspection unused*/
-public class AndroidWayTodayClient {
-    public final WayTodayClient client;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
+
+import kotlin.Unit;
+import s4y.gps.sdk.GPSUpdate;
+import s4y.gps.sdk.GPSUpdatesManager;
+import s4y.gps.sdk.android.GPSPowerManager;
+import s4y.gps.sdk.android.implementation.FusedGPSUpdatesProvider;
+import s4y.gps.sdk.dependencies.IGPSUpdatesProvider;
+
+/**
+ * @noinspection unused
+ */
+public class AndroidWayTodayClient implements Closeable {
+    public final WayTodayClient wtClient;
+    public final GPSUpdatesManager gpsUpdatesManager;
+
+    public boolean uploadLocationImmediately = true;
+    public final GPSPowerManager powerManager;
+
+    private final String provider;
+    private final Context context;
+    private final PreferenceOn trackingOn;
+
+    private static final HashMap<String, WayTodayClient> clients = new HashMap<>();
+
+    private final Data workerData = new Data.Builder()
+            .putString("clientID", UUID.randomUUID().toString())
+            .build();
+
+    public AndroidWayTodayClient(Context context, String principal, String secret, boolean tls, String host, int port, String provider, Looper looper) {
+        this.context = context;
+        this.provider = provider;
+        trackingOn = new PreferenceOn(context);
+
+        powerManager = new GPSPowerManager(context);
+
+        final GrpcClient grpcClient = new GrpcClient(principal, secret, tls, host, port, provider);
+        wtClient = new WayTodayClient(new PreferencesPersistedState(context), grpcClient);
+        String clientID = workerData.getString("clientID");
+        clients.put(clientID, wtClient);
+
+        IGPSUpdatesProvider gpsUpdatesProvider = new FusedGPSUpdatesProvider(context, looper);
+        gpsUpdatesManager = new GPSUpdatesManager(gpsUpdatesProvider, 500);
+        gpsUpdatesManager.getLast().addListener(this::gpsUpdatesListener);
+        if (trackingOn.get()) {
+            gpsUpdatesManager.start();
+        }
+    }
 
     public AndroidWayTodayClient(Context context, String principal, String secret, boolean tls, String host, int port, String provider) {
-        final GrpcClient grpcClient = new GrpcClient(principal, secret, tls, host, port, provider);
-        client = new WayTodayClient(new PreferencesPersistedState(context), grpcClient);
+        this(context, principal, secret, tls, host, port, provider, Looper.getMainLooper());
     }
 
     public AndroidWayTodayClient(Context context, String principal, String secret, String provider) {
-        this(context, principal, secret, true, "tracker.way.today", 9001, provider);
+        this(context, principal, secret, true, "tracker.way.today", 9101, provider);
     }
 
-    public class RequestTrackIdWorker extends Worker {
+    /** @noinspection RedundantThrows*/
+    @Override
+    public void close() throws IOException {
+        gpsUpdatesManager.getLast().removeListener(this::gpsUpdatesListener);
+        gpsUpdatesManager.close();
+    }
+
+    static public class RequestTrackIdWorker extends Worker {
+        static final Object workerClientLock = new Object();
+        static WayTodayClient workerClient;
+
         public RequestTrackIdWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
             super(context, workerParams);
         }
@@ -31,8 +89,13 @@ public class AndroidWayTodayClient {
         @NonNull
         @Override
         public Result doWork() {
-            final String tid = client.requestNewTrackerId(client.getCurrentTrackerId());
-            if (tid == null) {
+            String clientID = getInputData().getString("clientID");
+            WayTodayClient wtClient = clients.get(clientID);
+            if (wtClient == null) {
+                return Result.failure();
+            }
+            final String tid = wtClient.requestNewTrackerId(wtClient.getCurrentTrackerId());
+            if (tid.isEmpty()) {
                 return Result.failure();
             }
             final Data tidData = new Data.Builder()
@@ -42,7 +105,7 @@ public class AndroidWayTodayClient {
         }
     }
 
-    public class UploadLocationsWorker extends Worker {
+    static public class UploadLocationsWorker extends Worker {
         public UploadLocationsWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
             super(context, workerParams);
         }
@@ -50,8 +113,13 @@ public class AndroidWayTodayClient {
         @NonNull
         @Override
         public Result doWork() {
-            client.uploadLocations();
-            if (client.getUploadingLocationsStatus() == UploadingLocationsStatus.ERROR) {
+            String clientID = getInputData().getString("clientID");
+            WayTodayClient wtClient = clients.get(clientID);
+            if (wtClient == null) {
+                return Result.failure();
+            }
+            wtClient.uploadLocations();
+            if (wtClient.getUploadingLocationsStatus() == UploadingLocationsStatus.ERROR) {
                 return Result.failure();
             }
             return Result.success();
@@ -59,20 +127,75 @@ public class AndroidWayTodayClient {
     }
 
     public OneTimeWorkRequest createRequestTrackIdWorker() {
-        return new OneTimeWorkRequest.Builder(RequestTrackIdWorker.class).build();
+        return new OneTimeWorkRequest
+                .Builder(RequestTrackIdWorker.class)
+                .setInputData(workerData)
+                .build();
     }
 
+    /** @noinspection UnusedReturnValue*/
     public Operation enqueueTrackIdWorkRequest(Context context) {
-        OneTimeWorkRequest request = createUploadLocationsWorker();
+        OneTimeWorkRequest request = createRequestTrackIdWorker();
         return WorkManager.getInstance(context).enqueue(request);
     }
 
     public OneTimeWorkRequest createUploadLocationsWorker() {
-        return new OneTimeWorkRequest.Builder(UploadLocationsWorker.class).build();
+        return new OneTimeWorkRequest.Builder(UploadLocationsWorker.class)
+                .setInputData(workerData)
+                .build();
     }
 
+    /** @noinspection UnusedReturnValue*/
     public Operation enqueueUploadLocationsWorkRequest(Context context) {
         OneTimeWorkRequest request = createUploadLocationsWorker();
         return WorkManager.getInstance(context).enqueue(request);
+    }
+
+    private Unit gpsUpdatesListener(GPSUpdate gpsUpdate) {
+        String tid = wtClient.getCurrentTrackerId();
+        if (tid.isEmpty()) {
+            enqueueTrackIdWorkRequest(context);
+        } else {
+            wtClient.enqueueLocationToUpload(
+                    new Location(
+                            tid,
+                            gpsUpdate.getLatitude(),
+                            gpsUpdate.getLongitude(),
+                            0.0,
+                            (long) gpsUpdate.getBearing(),
+                            gpsUpdate.getTs(),
+                            0L, // TODO:
+                            false, //TODO:
+                            provider,
+                            gpsUpdate.getVelocity(),
+                            gpsUpdate.getAccuracy()
+                    ));
+            if (uploadLocationImmediately) {
+                enqueueUploadLocationsWorkRequest(context);
+            }
+        }
+        return Unit.INSTANCE;
+    }
+
+    public boolean isTrackingOn() {
+        return trackingOn.get();
+    }
+
+    public void enableTrackingOn() {
+        trackingOn.set(true);
+    }
+
+    public void turnTrackingOn() {
+        enableTrackingOn();
+        gpsUpdatesManager.start();
+    }
+
+    public void turnTrackingOff() {
+        this.trackingOn.set(false);
+        gpsUpdatesManager.stop();
+    }
+
+    static public boolean isTrackingOn(Context context) {
+        return new PreferenceOn(context).get();
     }
 }
--- waytoday-sdk/src/main/java/solutions/s4y/waytoday/sdk/PreferencesPersistedState.java
+++ waytoday-sdk/src/main/java/solutions/s4y/waytoday/sdk/PreferencesPersistedState.java
@@ -6,21 +6,24 @@ import androidx.annotation.NonNull;
 
  class PreferencesPersistedState implements IPersistedState {
     final private Context context;
+    private String trackerID;
 
     public PreferencesPersistedState(Context context) {
         this.context = context;
+        trackerID = context
+                .getSharedPreferences("solutions.s4y.waytoday.sdk", Context.MODE_PRIVATE)
+                .getString("tid", "");
     }
 
     @NonNull
     @Override
     public String getTrackerId() {
-        return context
-                .getSharedPreferences("solutions.s4y.waytoday.sdk", Context.MODE_PRIVATE)
-                .getString("tid", "");
+        return trackerID;
     }
 
     @Override
     public void setTrackerId(@NonNull String trackerID) {
+        this.trackerID = trackerID;
         context
                 .getSharedPreferences("solutions.s4y.waytoday.sdk", Context.MODE_PRIVATE)
                 .edit()
@@ -30,6 +33,6 @@ import androidx.annotation.NonNull;
 
      @Override
      public boolean hasTrackerId() {
-         return !getTrackerId().isEmpty();
+         return !this.trackerID.isEmpty();
     }
 }
